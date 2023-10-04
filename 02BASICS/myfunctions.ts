d773a742d169ecb9e8cb1145d2d7ca2b2ec998ac
Brandon <brancarp93@gmail.com>
2023-10-03 21:37:50 -0400

    typu functions

--- 02BASICS/myfunctions.ts
+++ 02BASICS/myfunctions.ts
@@ -1,8 +1,48 @@
-function addTwo(num: number){
-  
+function addTwo(num: number): number{
   return num + 2
+//  return "hello"
+}
+function getUpper(val: string) {
+  return val.toUpperCase()
 }
 
-addTwo(5)
+function signUpUser(name: string, email: string, isPaid:boolean ){
+}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+
+}
+
+
+let myValue = addTwo(5)
+getUpper("hello")
+
+signUpUser("baki", "hanma@gmail.com", true)
+
+loginUser("b", "b@b.com")
+
+// function getValue(myVal: number){
+//   if(myVal > 5) {
+//     return true
+//   } 
+//   return "200 OK"
+
+// }
+
+const getHello = (s: string):string => {
+    return ""
+}
+
+const heros = ["thor", "spiderman", "ironman"]
+// const heros = [1, 2, 3]
+
+heros.map(hero => {
+  return 'hero is ${hero}'
+})
+
+
+
+
+
 
 export {}
